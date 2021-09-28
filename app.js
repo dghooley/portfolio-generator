b0b4802cba3009818dd186f4f5d0dc51ad410745
Derick Hooley <dghooley@gmail.com>
2021-09-27 21:58:46 -0500

    codeSnapshotAlign

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
 
-const name = profileDataArgs[0];
+console.log(profileDataArgs);
 
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-/*
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+console.log(name, github);
 
-    console.log('================');
+const pageHTML = generatePage(name, github);
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-*/
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
