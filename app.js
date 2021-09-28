9627ebf2dc25b0d3ebdad37676fba195bb4bdbfc
Derick Hooley <dghooley@gmail.com>
2021-09-27 20:54:21 -0500

    codeSnapshotAlign

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-    // This..
+    // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
     }
 
-    console.log ('=============');
+    console.log('================');
 
-    // Is the same as this..
+    // Is the same as this...
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
