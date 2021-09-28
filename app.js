6892604c2c3d16761d3c5c9a6da367da226abd3d
Derick Hooley <dghooley@gmail.com>
2021-09-27 21:52:27 -0500

    addHTML

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
+const name = profileDataArgs[0];
+
+const github = profileDataArgs[1];
+
+/*
 const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -13,3 +22,10 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
